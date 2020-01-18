87e76262b6f2ba5cc6eafced7c70cfefab3c502d
rzh11111111 <294346639@qq.com>
2020-01-18 15:41:57 +0800

    组件

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import Button from './pages/Button/Button';
 
 function App() {
   return (
     <div className="App">
      <Home></Home>
+     <Button></Button>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,10 @@
 import React, {Component} from "react";
 export default class Home extends Component {
     render() {
-        return <div>Home</div>;
+        const ceshi = '11111'
+        return (<div>
+            <h1>Home</h1>
+            <h2>{ceshi}</h2>
+            </div>);
     }
 }

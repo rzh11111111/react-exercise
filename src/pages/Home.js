7f5d5ea45cdf2f38975058ffbd55e6c1864fab0c
rzh11111111 <294346639@qq.com>
2020-01-19 19:16:43 +0800

    组件生命周期

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,31 @@
 import React, {Component} from "react";
 export default class Home extends Component {
+    constructor(props){ //状态管理生命周期
+        super(props);
+        this.state= {
+            date:new Date()
+        }
+    }
+    //组件挂载完成之后
+    componentDidMount(){
+    //    this.timerId = setInterval(() => {
+    //         this.setState({
+    //             date:new Date()
+    //         })
+    //     }, 1000);
+    }
+    //组件卸载成功
+    componentWillUnmount(){
+        // clearInterval(this.timerId)
+    }
     render() {
         const ceshi = '11111'
+        const {date} = this.state
+        console.log(date,typeof date)
         return (<div>
             <h1>Home</h1>
             <h2>{ceshi}</h2>
+            <h3>{date.toString()+','+date.toLocaleDateString()}</h3>
             </div>);
     }
 }

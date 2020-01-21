3c52f0995ad291bfa1df2c638d094e561ed9b7bb
rzh11111111 <294346639@qq.com>
2020-01-21 20:00:03 +0800

    生命周期

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import './App.css';
 // import Home from './pages/Home';
 import Button from './pages/Button/Button';
 // import User from './pages/User';
-import Search from './pages/Search';
+// import Search from './pages/Search';
+import LifeCycle from './pages/LifeCycle';
 
 
-const store = {
-  userInfo:{
-    userName:'xiaoming'
-  }
-}
-function tellme(msg){
-  console.log('tellme',msg)
-}
+// const store = {
+//   userInfo:{
+//     userName:'xiaoming'
+//   }
+// }
+// function tellme(msg){
+//   console.log('tellme',msg)
+// }
 
 function App() {
   return (
     <div className="App">
      {/* <Home></Home> */}
      {/* <User></User> */}
-     <Search store={store} tellme={tellme}></Search>
+     {/* <Search store={store} tellme={tellme}></Search> */}
+     <LifeCycle></LifeCycle>
      <Button></Button>
     </div>
   );
---
+++ src/pages/LifeCycle.js
@@ -0,0 +1,48 @@
+import React , {Component} from 'react'
+export default class LifeCycle extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            counter:0,
+        };
+        console.log("constructor",this.state.counter)
+    }
+
+    componentWillMount(){
+        console.log("componentWillUnmount",this.state.counter)
+    }
+
+    componentDidMount(){
+        console.log("componentDidmount",this.state.counter)
+    }
+    componentWillUnmount(){
+        console.log("componentWillUnmount",this.state.counter)
+    }
+    componentWillUpdate(){
+        console.log("componentWillUpdate",this.state.counter)
+    }
+
+    componentDidUpdate(){
+        console.log("componentDidUpdate",this.state.counter)
+    }
+
+    shouldComponentUpdate(){
+        console.log("shouldComponentUpdate",this.state.counter)
+        return true;
+    }
+    setCounter=()=>{
+        this.setState({
+            counter:this.state.counter+1
+        })
+    }
+    render(){
+        console.log("render",this.state.counter)
+        const {counter}=this.state
+        return (
+            <div>LifeCycle
+                <p>{counter}</p>
+                <button onClick={this.setCounter}>改变counter</button>
+            </div>
+        )
+    }
+}

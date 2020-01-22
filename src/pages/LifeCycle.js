165b4c8a59845ac8d3dbd64920a25c31238b5a05
rzh11111111 <294346639@qq.com>
2020-01-22 15:33:38 +0800

    16.4更新的生命周期

--- src/pages/LifeCycle.js
+++ src/pages/LifeCycle.js
@@ -8,19 +8,32 @@ export default class LifeCycle extends Component {
         console.log("constructor",this.state.counter)
     }
 
-    componentWillMount(){
-        console.log("componentWillUnmount",this.state.counter)
+    // componentWillMount(){//即将废弃
+    //     console.log("componentWillUnmount",this.state.counter)
+    // }
+    static getDerivedStateFromProps(props,state){
+        //getDerivedStateFromProps 会在调用render方法之前调用
+        //并且在初始挂载及后续更新时都会被调用。
+        //它应返回一个对象来更新 state，如果返回null则不更新任何内容
+        const {counter} = state
+        console.log("getDerivedStateFromProps",counter)
+        return counter<8?null:{counter:0}
     }
-
+    getSnapshotBeforeUpdate(prevProps,prevState){
+        const {counter} = prevState;//上次的值
+        console.log("getSnapshotBeforeUpdate",counter);
+        return null;
+    }
     componentDidMount(){
         console.log("componentDidmount",this.state.counter)
     }
     componentWillUnmount(){
         console.log("componentWillUnmount",this.state.counter)
     }
-    componentWillUpdate(){
-        console.log("componentWillUpdate",this.state.counter)
-    }
+
+    // componentWillUpdate(){ 、、即将废弃
+    //     console.log("componentWillUpdate",this.state.counter)
+    // }
 
     componentDidUpdate(){
         console.log("componentDidUpdate",this.state.counter)
@@ -28,7 +41,8 @@ export default class LifeCycle extends Component {
 
     shouldComponentUpdate(nextProps,nextState){
         console.log("shouldComponentUpdate",this.state.counter,nextState.counter)
-        return true;//true的话就会render视图更新（值还是变），false的话render就不会更新，可以控制
+        return true;
+        //true的话就会render视图更新（值还是变），false的话render就不会更新，可以控制
     }
     setCounter=()=>{
         this.setState({

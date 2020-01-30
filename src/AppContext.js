dab21895c64b17e6628dafd92881cd7fe514794a
rzh11111111 <294346639@qq.com>
2020-01-30 11:34:26 +0800

    hoc

--- src/AppContext.js
+++ src/AppContext.js
@@ -3,7 +3,8 @@ const Context = React.createContext()
 export const Provider = Context.Provider
 export const Consumer = Context.Consumer
 
-//高阶组件Hoc
+//高阶组件Hoc+组件传值
+//定义：高阶组件是一个工厂函数，他接受一个组件并返回另一个组件
 export const consumerHandle = Cmp => props =>{
     return <Consumer>{ ctx=><Cmp {...props} {...ctx}></Cmp> }</Consumer>
     }
